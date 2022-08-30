71e4a862cb7bc4449e1c00b4822004630dcd4035
william-zhang-ml <william.zhang.ml@gmail.com>
2022-08-29 21:55:08 -0400

    added bottleneck residual layer

--- axon/components/residual.py
+++ axon/components/residual.py
@@ -83,3 +83,62 @@ class ResidualClassic(ResidualCustom):
             )
         )
     # pylint: enable=too-many-arguments
+
+
+class ResidualBottleneck(ResidualCustom):
+    """ Bottlenecked residual layer as per
+        'Deep Residual Learning for Image Recognition'
+        and
+        'Identity Mappings in Deep Residual Networks' """
+    # pylint: disable=too-many-arguments
+    def __init__(self,
+                 in_channels: int,
+                 bot_channels: int,
+                 kernel_size: Union[int, Tuple[int, int]] = 3,
+                 stride: Union[int, Tuple[int, int]] = 1,
+                 padding: Union[str, int, Tuple[int, int]] = 1,
+                 dilation: Union[int, Tuple[int, int]] = 1,
+                 groups: int = 1,
+                 bias: bool = False,
+                 norm_first: bool = True) -> None:
+        """
+        Define residual connection and initialize bottlenecked convolution.
+
+        Args:
+            in_channels:  number of input neurons/channels
+            bot_channels: number of neurons/channels after bottleneck
+            kernel_size:  kernel height and width
+            stride:       stride height and width, defaults to 1
+            padding:      row and col pixels to pad, defaults to 0
+            dilation:     space between kernel weights, defaults to 1
+            groups:       number of input groups, defaults to 1
+            bias:         whether conv layer uses bias terms, defaults to False
+            norm_first:   whether to batchnorm or relu first, defaults to True
+        """
+        super().__init__(
+            nn.Sequential(
+                ConvBlock(
+                    in_channels=in_channels,
+                    out_channels=bot_channels,
+                    kernel_size=1,
+                    bias=bias,
+                    norm_first=norm_first),
+                ConvBlock(
+                    in_channels=bot_channels,
+                    out_channels=bot_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    padding=padding,
+                    dilation=dilation,
+                    groups=groups,
+                    bias=bias,
+                    norm_first=norm_first),
+                ConvBlock(
+                    in_channels=bot_channels,
+                    out_channels=in_channels,
+                    kernel_size=1,
+                    bias=bias,
+                    norm_first=norm_first),
+            )
+        )
+    # pylint: enable=too-many-arguments

2eae7616b6287459bb7ac0d0c67ed0cdb4b7cd7e
william-zhang-ml <william.zhang.ml@gmail.com>
2022-09-05 14:27:43 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+""" Axon packages installation configuration. """
+from setuptools import setup
+
+
+setup(
+    name='axon',
+    version='0.0.1',
+    url="https://github.com/william-zhang-ml/axon",
+    author="William Zhang",
+    author_email="william.zhang.ml@gmail.com",
+    packages=['axon']
+)
